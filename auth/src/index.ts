755ef551a4c260becbb30c64314a004038df5889
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-22 16:57:14 +0100

    ingress-nginx config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,8 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req: any, res: any) => {
+  res.send('Hello world')
+})
+
 app.listen(3000, () => console.log('listening on 3000'))
