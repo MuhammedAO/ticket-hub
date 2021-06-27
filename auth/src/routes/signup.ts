917c8282371c81abc7eb6929266ef537f35b57de
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-28 00:40:30 +0100

    express validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from "express"
+import { body } from "express-validator"
 
 const router = express.Router()
 
-router.post('/api/users/signup', (req: any, res: any) => {
-  res.send('Hi there')
-})
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be a valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be 4-20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body
+  }
+)
 
-
-export {router as signupRouter}
-
-
-
+export { router as signupRouter }
