56d72ecc7bc66c2bbf3bb37fbafef919839a2e28
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-28 00:09:28 +0100

    scaffolding route handlers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,18 @@
+import { currentUserRouter } from "./routes/current-user"
+import { signinRouter } from "./routes/signin"
+import { signoutRouter } from "./routes/signout"
+import { signupRouter } from "./routes/signup"
+
 const express = require('express')
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/api/users/currentuser', (req: any, res: any) => {
-  res.send('Hello world')
-})
+app.use(currentUserRouter)
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+
 
 app.listen(3000, () => console.log('listening on 3000'))
