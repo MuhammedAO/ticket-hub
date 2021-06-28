498ce030b61c52be12c93e8656ac3b7d0debcfe3
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-28 22:46:13 +0100

    handling errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express"
-import { body } from "express-validator"
+import { body, validationResult } from "express-validator"
 
 const router = express.Router()
 
@@ -13,7 +13,16 @@ router.post(
       .withMessage("Password must be 4-20 characters"),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+      return res.status(400).send(errors.array())
+    }
     const { email, password } = req.body
+
+    console.log('creating user..')
+
+    res.send({})
   }
 )
 
