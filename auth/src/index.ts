995883b0eaef761ac011479adc56939c85396a31
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-29 09:35:18 +0100

    implement error middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
 import { signupRouter } from "./routes/signup"
+import { errorHandler } from './middlewares/error-handler';
 
 const express = require('express')
 
@@ -13,6 +14,8 @@ app.use(currentUserRouter)
 app.use(signupRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
+
+app.use(errorHandler)
 
 
 app.listen(3000, () => console.log('listening on 3000'))
