cbb1a3d4f4dd4619a6917d812730698ef0653289
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-29 10:58:47 +0100

    convert error to responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express"
+import { RequestValidationError } from "../errors/request-validation-error"
+import { DatabaseConnectionError } from "../errors/database-connection-error"
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,18 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(err)
+  if (err instanceof RequestValidationError) {
+    const formattedError = err.errors.map((error) => {
+      return { message: error.msg, field: error.param }
+    })
 
+    return res.status(400).send({ errors: formattedError })
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] })
+  }
   res.status(400).send({
-    message: err.message
+    errors: [{message: 'Something went wrong'}]
   })
 }
