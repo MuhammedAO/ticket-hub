3141719aeeeb9faf1cd9c5a368739aa960965959
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-30 07:29:05 +0100

    handling async errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,11 @@
+import express from "express"
+import 'express-async-errors'
 import { currentUserRouter } from "./routes/current-user"
 import { signinRouter } from "./routes/signin"
 import { signoutRouter } from "./routes/signout"
 import { signupRouter } from "./routes/signup"
-import { errorHandler } from './middlewares/error-handler';
-
-const express = require('express')
+import { errorHandler } from "./middlewares/error-handler"
+import { NotFoundError } from "./errors/not-found-error"
 
 const app = express()
 
@@ -15,7 +16,10 @@ app.use(signupRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 
+app.all("*", async (req: any, res: any) => {
+  throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
-
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(3000, () => console.log("listening on 3000"))
