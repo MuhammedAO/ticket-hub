803e789270d1c923ad70a98b0df662753c790743
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-30 06:53:19 +0100

    abstracting custom error

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,16 +1,14 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error"
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500
-  reason = 'Error connecting to DB'
-  constructor(){
-    super()
+  reason = "Error connecting to DB"
+  constructor() {
+    super('Not connected to DB')
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
-
   }
- 
-  serializeErrors(){
-    return [
-      {message: this.reason}
-    ]
+
+  serializeErrors() {
+    return [{ message: this.reason }]
   }
 }
