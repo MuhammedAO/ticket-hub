58250713521ad8f4a042e9b37d10f136fe6612ca
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-06-30 06:25:56 +0100

    serialize error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,16 @@
 import { ValidationError } from "express-validator"
 
 export class RequestValidationError extends Error {
+  statusCode = 400
   constructor(public errors: ValidationError[]) {
     super()
 
     Object.setPrototypeOf(this, RequestValidationError.prototype)
+  }
+
+  serializeErrors(){
+    return this.errors.map(err => {
+      return {message: err.msg, field: err.param}
+    })
   }
 }
