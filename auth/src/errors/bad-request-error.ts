49f0017897e43f14c36c4ce61a9c99ed9a62f136
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-01 21:55:57 +0100

    custom error for bad requests

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,13 @@
+import { CustomError } from "./custom-error"
+
+export class BadRequestError extends CustomError {
+  statusCode = 400
+  constructor(public msg: string) {
+    super(msg)
+    Object.setPrototypeOf(this, BadRequestError.prototype)
+  }
+
+  serializeErrors() {
+    return [{ message: this.msg }]
+  }
+}
