f24b55b0498204c72c74b4f044bf783c80f50e2c
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-02 10:42:46 +0100

    mongoose pres-save hook

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import { Password } from '../services/password';
 
 //properties required to create a new user
 interface UserAttrs {
@@ -28,6 +29,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+})
+
+userSchema.pre('save', async function(done) {
+  
+  if(this.isModified('password')) {
+   const hashed = await Password.toHash(this.get('password'))
+
+   this.set('password', hashed)
+  }
+
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
