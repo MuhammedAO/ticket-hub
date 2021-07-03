fd8ca2fd8ea9cfa92fe0491d3cdbd656f6d14ac6
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-03 21:14:16 +0100

    req validation middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express"
-import { body, validationResult } from "express-validator"
+import { body } from "express-validator"
 import jwt from 'jsonwebtoken'
 
-import { RequestValidationError } from "../errors/request-validation-error"
+
 import { User } from "../models/user"
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router()
 
@@ -21,12 +22,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be 4-20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array())
-    }
 
     const { email, password } = req.body
 
