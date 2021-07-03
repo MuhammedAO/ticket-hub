0e8973c4e5d62402aa449fdf89b8fde39e9d52bc
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-03 21:36:01 +0100

    signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express"
 import { body } from "express-validator"
+import jwt from "jsonwebtoken"
+
 import { validateRequest } from "../middlewares/validate-request"
+import { User } from "../models/user"
+import { BadRequestError } from "../errors/bad-request-error"
+import { Password } from "../services/password"
 
 const router = express.Router()
 
@@ -14,7 +19,37 @@ router.post(
       .withMessage("You must provide a provide"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials")
+    }
+
+    const passwordMatch = Password.compare(existingUser.password, password)
+
+    if (!passwordMatch) {
+      throw new BadRequestError("invalid credentials")
+    }
+
+    //generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    )
+
+    //store it on the session{}
+    req.session = {
+      jwt: userJwt,
+    }
+
+    res.status(200).send(existingUser)
+  }
 )
 
 export { router as signinRouter }
