b6307c76dbba9af7c61748e85f32d3fd7378ead9
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-03 22:56:56 +0100

    signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
-router.post('/api/users/signout', (req: any, res: any) => {
-  res.send('Hi there')
+router.post('/api/users/signout', (req: Request, res: Response) => {
+  req.session = null
+
+  res.send({})
 })
 
 
