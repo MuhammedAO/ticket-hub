0f6f690302232e82f44298ddfcc2a1cfd10e6866
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-06 15:26:18 +0100

    refactor to accomodate supertest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from "express"
-import 'express-async-errors'
-import cookieSession from 'cookie-session'
-
-import { currentUserRouter } from "./routes/current-user"
-import { signinRouter } from "./routes/signin"
-import { signoutRouter } from "./routes/signout"
-import { signupRouter } from "./routes/signup"
-import { errorHandler } from "./middlewares/error-handler"
-import { NotFoundError } from "./errors/not-found-error"
+import { app } from './app';
 import mongoose from 'mongoose'
-
-const app = express()
-app.set('trust proxy', true)
-app.use(express.json())
-
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-)
-
-app.use(currentUserRouter)
-app.use(signupRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-
-app.all("*", async (req: any, res: any) => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
 
 const start = async () => {
   if(!process.env.JWT_KEY) {
