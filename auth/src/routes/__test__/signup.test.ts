f0b7ea1a61eca34650dc892561627d83dc78b0b5
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-07 04:22:34 +0100

    Tests

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,54 @@
+import request from "supertest"
+import { app } from "../../app"
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "random@test.com",
+      password: "random",
+    })
+    .expect(201)
+})
+
+it("it returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "random",
+      password: "random",
+    })
+    .expect(400)
+})
+
+it("it returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "random@me.com",
+      password: "ran",
+    })
+    .expect(400)
+})
+
+it("it returns a 400 with an missing email and password", async () => {
+  return request(app).post("/api/users/signup").send({}).expect(400)
+})
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+        .post("/api/users/signup")
+        .send({
+          email: "random@test.com",
+          password: "random",
+        })
+
+        .expect(201)
+  await request(app)
+        .post("/api/users/signup")
+        .send({
+          email: "random@test.com",
+          password: "random",
+        })
+        .expect(400)
+})
