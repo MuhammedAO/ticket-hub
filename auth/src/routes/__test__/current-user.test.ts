f9ca6988fb2505f01d3f9d365b380c367d29006a
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-07 14:30:41 +0100

    testing not-auth requests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,17 +1,8 @@
 import request from "supertest"
 import { app } from "../../app"
-import { currentUser } from "../../middlewares/current-user"
 
 it("responds with details about the current user", async () => {
-  const authResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "random@test.com",
-      password: "random",
-    })
-    .expect(201)
-
-  const cookie = authResponse.get("Set-Cookie")
+  const cookie = await global.signin()
 
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -20,4 +11,14 @@ it("responds with details about the current user", async () => {
     .expect(200)
 
   expect(response.body.currentUser.email).toEqual("random@test.com")
+})
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200)
+
+  expect(response.body.currentUser).toEqual(null)
+
 })
