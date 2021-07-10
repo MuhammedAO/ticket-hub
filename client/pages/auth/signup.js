1a406afbfe4b6eca9c5219d3d30c3599ffe05706
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-10 01:18:32 +0100

    sign up

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,51 +1,79 @@
-import React, {useState} from 'react'
-import FormContainer from '../../components/FormContainer'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import Link from 'next/link'
+import React, { useState } from "react"
+import FormContainer from "../../components/FormContainer"
+import Link from "next/link"
+import { Form, Button, Row, Col } from "react-bootstrap"
+import axios from "axios"
+import Message from "../../components/Message"
+import Loader from "../../components/Loader"
 
 function SignUp() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  const onSubmit = (e) => {
+
+  const onSubmit = async (e) => {
     e.preventDefault()
-    console.log(email, password)
+
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      })
+      setLoading(true)
+      setEmail('')
+      setPassword('')
+    
+      console.log(response.data)
+    } catch (err) {
+      setErrors(err.response.data.errors)
+    }
+
+    setLoading(false)
   }
 
   return (
     <FormContainer>
       <h1>Sign up</h1>
+        {errors.length > 0 &&
+          errors.map((err) => (
+            <Message key={err.message} variant="danger">
+              {err.message}
+            </Message>
+          ))}
+          {loading && <Loader/>}
       <Form onSubmit={onSubmit}>
-        <Form.Group controlId='email'>
+        <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type='email'
-            placeholder='Enter your email'
+            type="email"
+            placeholder="Enter your email"
             value={email}
-            onChange={e => setEmail(e.target.value)}
-          >
-          </Form.Control>
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='password'>
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Enter your password'
+            type="password"
+            placeholder="Enter your password"
             value={password}
-            onChange={e => setPassword(e.target.value)}
-          >
-          </Form.Control>
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>Register</Button>
+        <Button type="submit" variant="primary">
+          Register
+        </Button>
       </Form>
       <Row className="py-3">
         <Col>
           Have an account?
           <Link href="/">
-          <a> Login</a>
-        </Link>
+            <a> Login</a>
+          </Link>
         </Col>
       </Row>
     </FormContainer>
