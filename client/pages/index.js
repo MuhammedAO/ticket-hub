4b3b3e273de5b9af3321d1e77a610e1850c2ef9e
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-10 14:22:04 +0100

    fetching data in ssr :(

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react"
+import axios from "axios"
 
-function Home() {
+function Home({ currentUser }) {
+  console.log(currentUser)
   return (
     <div>
       <h1>Hello world</h1>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(
+    "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+    {
+      headers: {
+        Host: "ticketing.dev",
+      },
+    }
+  )
+  return {
+    props: {
+      currentUser: data,
+    },
+  }
 }
 
 export default Home
