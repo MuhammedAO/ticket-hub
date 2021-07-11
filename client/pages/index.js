6141678772670467cfa954e6ea4eca23d3c03b16
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-11 17:11:40 +0100

    auth user content

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,23 +2,18 @@ import React from "react"
 import axios from "axios"
 
 function Home({ currentUser }) {
-  console.log(currentUser)
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="container">
+    { currentUser ?  <h5>Welcome {currentUser.email} You are now signed in</h5> : <h3>Welcome Guest. Please Sign in to continue</h3>}
     </div>
   )
 }
 
 export const getServerSideProps = async ({ req }) => {
-  console.log(req)
   const { data } = await axios.get(
     "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
     {
-      headers: {
-        Host: "ticketing.dev",
-        cookie: req.headers.cookie,
-      },
+      headers: req.headers
     }
   )
   return {
@@ -27,5 +22,6 @@ export const getServerSideProps = async ({ req }) => {
     },
   }
 }
+
 
 export default Home
