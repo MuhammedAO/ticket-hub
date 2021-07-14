b939c33d40f166727eee6c2ce6cf88252d86513f
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-15 00:30:04 +0100

    tests

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,74 @@
+import request from "supertest"
+import { app } from "../../app"
+import { Ticket } from "../../models/ticket"
+
+it("has a route handler listening to /api/tickets for post requests", async () => {
+  const response = await request(app).post("/api/tickets").send({})
+  expect(response.status).not.toEqual(404)
+})
+
+it("can only be accessed if a user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401)
+})
+
+it("status should !== 401 if the user is signed in", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({})
+  expect(response.status).not.toEqual(401)
+})
+
+it("returns an error if invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 20,
+    })
+    .expect(400)
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 20,
+    })
+    .expect(400)
+})
+it("returns an error if invalid price is provided ", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "Opera",
+      price: -10,
+    })
+    .expect(400)
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "Opera",
+    })
+    .expect(400)
+})
+
+it("creates a ticket with valid parameters", async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "Opera",
+      price: 34,
+    })
+    .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+})
