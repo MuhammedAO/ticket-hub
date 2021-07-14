3bf4a811c5692955c4b5e92042bc055ef7ed041c
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-14 12:15:13 +0100

    tickets~mongo connect

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,8 +5,12 @@ const start = async () => {
   if(!process.env.JWT_KEY) {
     throw new Error('jwt not defined')
   }
+  if(!process.env.MONGO_URI) {
+    throw new Error('cannot find mongo uri connect string')
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
@@ -17,7 +21,7 @@ const start = async () => {
     console.error(error)
   }
 
-  app.listen(3000, () => console.log("listening on port 3000"))
+  app.listen(3000, () => console.log("Ticket srv listening on port 3000"))
 }
 
 start()
