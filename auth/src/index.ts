36f392331daa94ad7670aebebda792920b85cad0
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-14 12:13:37 +0100

    env for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if(!process.env.JWT_KEY) {
     throw new Error('jwt not defined')
   }
+  if(!process.env.MONGO_URI) {
+  throw new Error("cannot find uri connection string")
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
