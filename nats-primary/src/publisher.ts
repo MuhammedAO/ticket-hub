6471273bc8a8af3dc610b47ca8bc2c93748c39e8
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-17 15:38:49 +0100

    nats test

---
+++ nats-primary/src/publisher.ts
@@ -0,0 +1,18 @@
+import nats from 'node-nats-streaming'
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222'
+})
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS')
+
+  const data = JSON.stringify({
+    id:'jjk',
+    title: 'Hello world',
+    price:23
+  })
+
+  stan.publish('ticked:created', data, () => console.log('Event published'))
+
+})
