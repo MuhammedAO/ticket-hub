c1939bde4536c9e95d8c86ac663b5105b934dc19
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-18 13:02:26 +0100

    nats subscription options

--- nats-primary/src/subscriber.ts
+++ nats-primary/src/subscriber.ts
@@ -10,12 +10,25 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Subscriber connected to NATS")
 
-  const options = stan.subscriptionOptions().setManualAckMode(true)
+  stan.on("close", () => {
+    console.log("Connection close")
+    process.exit()
+  })
+
+  const options = stan
+  .subscriptionOptions()
+  // Configures the subscription to require manual acknowledgement of messages using Message#acknowledge.
+  .setManualAckMode(true)
+  // Configures the subscription to replay from first available message.
+  .setDeliverAllAvailable()
+  // Sets a durable subscription name that the client can specify for the subscription. This enables the subscriber to close the connection without canceling the subscription and resume the subscription with same durable name. Note the server will resume the subscription with messages that have not been acknowledged.
+  .setDurableName('a-service')
 
   // Subscribes to a given subject as an optional member of a queue group.
+  // the qGrp makes sure that we do not accidentally dump the durable name even if all of our  services restarts and to make sure that emmitted events only go off to 1 instance of the services
   const subscription = stan.subscribe(
     "ticked:created",
-    "orders-service-queue-group",
+    // "orders-service-queue-group",
     options
   )
 
@@ -32,3 +45,6 @@ stan.on("connect", () => {
     msg.ack()
   })
 })
+
+process.on("SIGINT", () => stan.close())
+process.on("SIGTERM", () => stan.close())
