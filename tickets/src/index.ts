e1aa9a2228fd7a3eb735c3f8d267cddec24c4ff8
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-19 23:07:07 +0100

    singleton implementation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,22 +1,24 @@
-import { app } from './app';
-import mongoose from 'mongoose'
+import { app } from "./app"
+import mongoose from "mongoose"
+import { natsWrapper } from "./nats-wrapper"
 
 const start = async () => {
-  if(!process.env.JWT_KEY) {
-    throw new Error('jwt not defined')
+  if (!process.env.JWT_KEY) {
+    throw new Error("jwt not defined")
   }
-  if(!process.env.MONGO_URI) {
-    throw new Error('cannot find mongo uri connect string')
+  if (!process.env.MONGO_URI) {
+    throw new Error("cannot find mongo uri connect string")
   }
 
   try {
+    await natsWrapper.connect("ticketing", "jnj", "http://nats-srv:4222")
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     })
 
-    console.log('DB connected')
+    console.log("DB connected")
   } catch (error) {
     console.error(error)
   }
