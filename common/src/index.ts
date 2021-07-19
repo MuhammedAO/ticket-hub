595954559b0227c0c93d4b17b8455ffae7d4a7b2
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-19 17:44:59 +0100

    missing file from export

--- common/src/index.ts
+++ common/src/index.ts
@@ -13,6 +13,6 @@ export * from './middlewares/validate-request'
 export * from "./events/base-listener"
 export * from "./events/base-publisher"
 export * from "./events/subjects"
-export * from "./events/ticket-uptated-event"
+export * from "./events/ticket-created-event"
 export * from "./events/ticket-uptated-event"
 
