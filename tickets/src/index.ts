614073ac32e78bda268cf86c260b8bf73e9dfddf
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-20 00:23:05 +0100

    close nats conn

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,13 @@ const start = async () => {
 
   try {
     await natsWrapper.connect("ticketing", "jnj", "http://nats-srv:4222")
+    natsWrapper.client.on("close", () => {
+      console.log("Connection close")
+      process.exit()
+    })
+
+    process.on("SIGINT", () => natsWrapper.client.close())
+    process.on("SIGTERM", () => natsWrapper.client.close())
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
