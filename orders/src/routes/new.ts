08f78ee8c0104f79b6c1d21b6a78f38b816e4356
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-21 18:59:50 +0100

    order creation logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,45 @@
 import express, { Request, Response } from "express"
+import mongoose from "mongoose"
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+} from "@mhd-ticketx/ticket-x"
+import { body } from "express-validator"
+import { Ticket } from "../models/ticket"
+import { Order } from "../models/order"
 
 const router = express.Router()
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({})
-})
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId)
+    if (!ticket) {
+      throw new NotFoundError()
+    }
 
+    // Make sure that this ticket is not already reserved
 
-export {router as newOrderRouter}
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
+    res.send({})
+  }
+)
+
+export { router as newOrderRouter }
