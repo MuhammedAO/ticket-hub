6d6d1ec20533ee189db188f04b6bb6ab1d43e624
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-22 06:35:49 +0100

    order expiration

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,6 +13,9 @@ import { Order } from "../models/order"
 
 
 const router = express.Router()
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
+
 router.post(
   "/api/orders",
   requireAuth,
@@ -44,11 +47,22 @@ router.post(
 
     // Calculate an expiration date for this order
 
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
+    })
+
+    await order.save()
 
     // Publish an event saying that an order was created
 
-    res.send({})
+    res.status(201).send(order)
   }
 )
 
