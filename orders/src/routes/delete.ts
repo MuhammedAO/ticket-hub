cf7a370515eb45472b012c1aa6ab512eff836349
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-22 11:03:10 +0100

    delete an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,11 +1,32 @@
 import express, { Request, Response } from "express"
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorizedError,
+} from "@mhd-ticketx/ticket-x"
+import { Order, OrderStatus } from "../models/order"
 
 const router = express.Router()
 
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({})
-})
+    const order = await Order.findById(orderId)
 
+    if (!order) {
+      throw new NotFoundError()
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError("Not Authorized")
+    }
+    order.status = OrderStatus.Cancelled
+    await order.save()
 
-export {router as deleteOrderRouter}
+    res.status(204).send(order)
+  }
+)
+
+export { router as deleteOrderRouter }
