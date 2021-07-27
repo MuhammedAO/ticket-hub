202f5b4543b7e614c855d543ba0a9d5178faa0a2
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-27 12:18:39 +0100

    reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,27 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@mhd-ticketx/ticket-x"
 import { queueGroupName } from "./queue-group-name"
 import { Message } from "node-nats-streaming"
+import { Ticket } from "../../models/ticket"
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated
   queueGroupName = queueGroupName
 
- async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    //find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id)
 
+    //if !ticket, throw err
+    if (!ticket) {
+      throw new Error("Ticket not found")
+    }
+    //mark ticket as being reserved by setting its order prop
+    ticket.set({ orderId: data.id })
+
+    //save the ticket
+    await ticket.save()
+
+    //ack the message
+    msg.ack()
   }
 }
