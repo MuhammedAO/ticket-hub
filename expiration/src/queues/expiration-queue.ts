9fe9c1a6a44623893cae4953bed62804ba88b97e
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-29 17:49:28 +0100

    processing job

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,9 +1,10 @@
 import Queue from "bull"
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher"
+import { natsWrapper } from "../nats-wrapper"
 
 interface Payload {
   orderId: string
 }
-
 
 const expirationQueue = new Queue<Payload>("order:expiration", {
   redis: {
@@ -13,7 +14,9 @@ const expirationQueue = new Queue<Payload>("order:expiration", {
 
 //process job
 expirationQueue.process(async (job) => {
-  console.log("publish expiration:complete event for orderId", job.data.orderId)
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  })
 })
 
 export { expirationQueue }
