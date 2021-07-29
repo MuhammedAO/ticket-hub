21ffb74e180baefd3f8f8ae728f625b1cb56927e
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-29 18:31:55 +0100

    dont cancel completed events

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -20,9 +20,14 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       throw new Error("Order not found")
     }
 
+    if(order.status === OrderStatus.Complete) {
+      return msg.ack()
+    }
+
     order.set({
       status: OrderStatus.Cancelled,
     })
+
     await order.save()
 
     await new OrderCancelledPublisher(this.client).publish({
