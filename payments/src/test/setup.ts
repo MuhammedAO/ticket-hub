b1d1f8440e891e67f8f96f6e55467afd6641a573
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-07-30 12:40:45 +0100

    initial setup

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,65 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[]
+    }
+  }
+}
+
+jest.mock('../nats-wrapper.ts')
+
+let mongo: any
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "jhknj"
+  mongo = await MongoMemoryServer.create()
+  const mongoUri = mongo.getUri()
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+beforeEach(async () => {
+  jest.clearAllMocks()
+  const collections = await mongoose.connection.db.collections()
+
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+global.signin = () => {
+  //fake authentication
+  //build jwt payload
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "llkmfk@kmdkl.com",
+  }
+
+  //create jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+
+  //build session{}
+  const session = {
+    jwt: token,
+  }
+  //convert to json
+  const sessJSON = JSON.stringify(session)
+
+  //encode
+  const base64 = Buffer.from(sessJSON).toString("base64")
+
+  return [`express:sess${base64}`]
+}
